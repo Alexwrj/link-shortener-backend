4f788319879a6bb6c72a38230928ff6d5158a845
Alexwrj <hunterfad@gmail.com>
2020-06-14 15:08:29 +0300

    Add user alias for shorten link

--- routes/links/short_link.post.js
+++ routes/links/short_link.post.js
@@ -9,17 +9,31 @@ inputValidator(
       type: 'STRING',
       required: true,
       pattern: /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
-    }
+    },
+
+    alias: {
+      type: 'STRING',
+      pattern: /^[a-z0-9-_]+$/i,
+    },
   }
 ),
 
-async (req, res) => {
-  const { origin } = req.body;
+async (req, res, next) => {
+  const { origin, alias } = req.body;
   const models = res.app.get('models');
 
   const newLink = await models.Link.create({ origin });
 
-  const shorten = base62.encode(newLink.id);
+  let shorten = '';
+
+  if (alias) {
+    const existingLink = await models.Link.findOne({ where: { shorten: alias } });
+    if (existingLink) res.status(409).json({ message: 'Short link with this name already exists.' });
+
+    shorten = alias;
+  } else {
+    shorten = base62.encode(newLink.id);
+  }
 
   newLink.update({ shorten });
 
